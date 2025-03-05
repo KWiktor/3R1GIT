78fd5bdb5aa88d1c7a67578c182b6310123b9d2d
KWiktor <wiktor.krzciuk@gmail.com>
2025-03-05 13:41:05 +0100

    egzamin

---
+++ organizer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organizer</title>
+</head>
+<body>
+    <div id="baner">
+        <div id="lewo"></div>
+        <div id="srodek"></div>
+        <div id="prawo"></div>
+    </div>    
+
+    <div id="main"></div>
+    
+
+
+
+</body>
+</html>
